9aec933202af294cef01db45058fc17aec0048f7
ela-PC\ela <elatiwi59@gmail.com>
2017-05-25 12:31:19 +0700

    V edit lowongan

--- application/views/perusahaan/v_edit_lowongan.php
+++ application/views/perusahaan/v_edit_lowongan.php
@@ -1,13 +1,13 @@
- <!DOCTYPE html>
+  <!DOCTYPE html>
 <html lang="">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Admin Page</title>
+		<meta charset="utf-8"> 
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>Admin Page</title> 
 
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="<?php echo base_url('') ?>assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url('') ?>assets/css/bootstrap.min.css"> 
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,7 +19,7 @@
 	<body>
 		<div class="container-fluid">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<nav class="navbar navbar-default" role="navigation">
+						<nav class="navbar navbar-default" role="navigation"> 
 							<div class="container-fluid">
 								<!-- Brand and toggle get grouped for better mobile display -->
 								<div class="navbar-header">
