98291167a0488b9fdc96afdfc5e4647f902ea029
NOTEBOOK-PC\Netbook <dhayyu6@gmail.com>
2017-05-28 22:32:36 +0700

    proses model juga berubah

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -5,43 +5,21 @@ class m_login extends CI_Model
 {
 	public function login($Username, $Password)
 	{
-		$this->db->select('ID_ADMIN, NAMA_ADMIN, PASSWORD');
-		$this->db->from('admin');
+		//$this->db->select('ID_ADMIN, NAMA_ADMIN, PASSWORD');
+		//$this->db->from('admin');
 		$this->db->where('NAMA_ADMIN', $Username);
-		$this->db->where('PASSWORD', MD5($Password));
-		$query=$this->db->get();
-		if ($query->num_rows()==1)
-		{
-			return $query->result();
-		}
-		else 
-		{
-			return false;
-		}
+		$this->db->where('PASSWORD', $Password);
+		return $this->db->get('admin');
+		//$query=$this->db->get();
+		//if ($query->num_rows()==1)
+		//{
+		//	return $query->result();
+		//}
+		//else 
+		//{
+		//	return false;
+		//}
 		
-	}
-}
-
-	/*public function insertUser()
-	{
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$password_encrypt=md5($password);
-
-		$query = $this->db->query("INSERT INTO user VALUES ('', '$username', '$password_encrypt')");
-	}
-
-	function isUsernameExist($username) {
-	    $this->db->select('id');
-	    $this->db->from('user');
-	    $this->db->where('username', $username);
-	    $query = $this->db->get();
-
-	    if ($query->num_rows() > 0) {
-	        return true;
-	    } else {
-	        return false;
-	    }
 	}
 }
 
