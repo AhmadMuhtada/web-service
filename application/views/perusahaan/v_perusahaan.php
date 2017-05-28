a7141e3f1a8e50a525357253617e941c2727b2d3
NOTEBOOK-PC\Netbook <dhayyu6@gmail.com>
2017-05-28 23:07:45 +0700

    view perusahaan berubah

--- application/views/perusahaan/v_perusahaan.php
+++ application/views/perusahaan/v_perusahaan.php
@@ -1,90 +1,63 @@
-<!DOCTYPE html>
-<html lang="">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Admin Page</title>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>OVA | Administrator</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="<?php echo base_url('') ?>assets/css/bootstrap.min.css">
-
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
-		<div class="container-fluid">
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<nav class="navbar navbar-default" role="navigation">
-							<div class="container-fluid">
-								<!-- Brand and toggle get grouped for better mobile display -->
-								<div class="navbar-header">
-								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-										<span class="sr-only">Toggle navigation</span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</button>
-									<a class="navbar-brand" href="<?php echo site_url('home_admin') ?>">Home</a>
-								</div>
-						
-								<!-- Collect the nav links, forms, and other content for toggling -->
-								<div class="collapse navbar-collapse navbar-ex1-collapse">
-									<ul class="nav navbar-nav">
-										<li class="dropdown">
-											<a href="<?php site_url('') ?>" class="dropdown-toggle" data-toggle="dropdown">Perusahaan<b class="caret"></b></a>
-											<ul class="dropdown-menu">
-											    <li><a href="<?php echo site_url('perusahaan')?>">List Perusahaan</a></li>
-												<li><a href="#">Lowongan</a></li>
-												
-											</ul>
-										</li>
-									</ul>
-									<ul class="nav navbar-nav">
-										<li class="dropdown">
-											<a href="<?php site_url('') ?>" class="dropdown-toggle" data-toggle="dropdown">Pelamar<b class="caret"></b></a>
-											<ul class="dropdown-menu">
-												<li><a href="<?php echo site_url('pelamar')?>">List Pelamar</a></li>
-												<li><a href="#">Form Pelamar</a></li>
-												
-											</ul>
-										</li>
-									</ul>
-									<ul class="nav navbar-nav">
-									<li class="button">
-											<li class="button"><a href="<?php echo site_url('home_admin/create')?>">Tambah Admin</a></li>
-											</li>
-									</ul>
-									<ul class="nav navbar-nav navbar-right">
-										<li class="active">
-											<li class="active"><a href="<?php echo site_url('')?>">Log Out</a></li>
-											<!--<ul class="dropdown-menu">
-												<li><a href="#">Pemrograman</a></li>
-												<li><a href="#">Web</a></li>
-												<li><a href="#">Berbasis</a></li>
-												<li><a href="#">Framework</a></li>
-											</ul>-->
-										</li>
-									</ul>
-								</div><!-- /.navbar-collapse -->
-						</div>
-						</nav>
-
-					</div>	
-
-
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-						<h1>List Perusahaan</h1>	
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('') ?>assets/css/style.css">
+		<script type="text/javascript" src="<?php echo base_url('') ?>assets/Query-2.2.4/jquery-2.2.4.min.js"></script>
+</head>
+<body>
+<div id="wrapper">
+  <div class="overlay"></div>
+  
+  <!-- Sidebar -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+    <ul class="nav sidebar-nav">
+      <li class="sidebar-brand"> <a href="#"> OVA ! </a> </li>
+      <li> <a href="#"><i class="fa fa-fw fa-home"></i> Beranda </a> </li>
+      <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-fw fa-file-o"></i> Data <span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="<?php echo site_url('home_admin') ?>">List Admin</a></li>
+          <li><a href="<?php echo site_url('perusahaan') ?>">List Perusahaan</a></li>
+          <li><a href="<?php echo site_url('pelamar') ?>">List Pelamar</a></li>
+          <li><a href="<?php echo site_url('lowongan') ?>">List Lowongan</a></li>
+          <li><a href="<?php echo site_url('form') ?>">List Form Pelamar</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-fw fa-plus"></i> Entri Data <span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="<?php echo site_url('home_admin/create') ?>">Tambah Admin</a></li>
+          <li><a href="<?php echo site_url('perusahaan/create') ?>">Tambah Perusahaan</a></li>
+		  <li><a href="<?php echo site_url('lowongan/create') ?>">Tambah Lowongan</a></li>
+ 
+        </ul>
+      </li>
+	  <li> <a href="<?php echo site_url('Login/logout') ?>"><i class="fa fa-fw fa-cog"></i> Logout</a> </li>
+    </ul>
+  </nav>
+  
+  <!-- /#sidebar-wrapper --> 
+  
+  <!-- Page Content -->
+  <div id="page-content-wrapper">
+    <button type="button" class="hamburger is-closed animated fadeInLeft" data-toggle="offcanvas"> <span class="hamb-top"></span> <span class="hamb-middle"></span> <span class="hamb-bottom"></span> </button>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+          <h1 class="page-header">List Perusahaan</h1>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="table-responsive">
 							<table class="table table-hover" id="example">
 								<thead>
 									<tr>
 									<th>No</th>
-									<th>Id Perusahaan</th> 
+									<th>Id Perusahaan</th>
 									<th>Nama Perusahaan</th>
 									<th>Alamat Perusahaan</th>
 									<th>Info Perusahaan</th>
@@ -105,11 +78,11 @@
 										<td><?php echo $no ?></td>
 										<td><?php echo $p->ID_PERUSAHAAN ?></td>
 										<td><?php echo $p->NAMA_PERUSAHAAN ?></td>
-										<td><?php echo $p->ALAMAT_PERUSAHAAN ?></td> 
-										<td><?php echo $p->INFO_PERUSAHAAN ?></td> 
-										<td><?php echo $p->EMAIL_PERUSAHAAN ?></td> 
-										<!--<td><?php echo $p->PASSWORD_PERUSAHAAN ?> </td>-->
-										<td><?php echo $p->NO_TELP_PERUSAHAAN ?></td> 
+										<td><?php echo $p->ALAMAT_PERUSAHAAN ?></td>
+										<td><?php echo $p->INFO_PERUSAHAAN ?></td>
+										<td><?php echo $p->EMAIL_PERUSAHAAN ?></td>
+										<!--<td><?php echo $p->PASSWORD_PERUSAHAAN ?></td>-->
+										<td><?php echo $p->NO_TELP_PERUSAHAAN ?></td>
 										<!--<td><?php echo $p->FOTO_PERUSAHAAN ?></td>-->
 
 										<td><img width="100" height="100" src="<?=base_url()?>assets/foto/perusahaan/<?=$p->FOTO_PERUSAHAAN?>"></td>
@@ -127,11 +100,44 @@
 					</div>
 
 
-					<!-- jQuery -->
+		<!-- jQuery Bootstrap -->
 		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script> 
+		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> 
 		<!-- Bootstrap JavaScript -->
-		<script src="<?php echo base_url('') ?>assets/js/bootstrap.min.js"></script>
+		<!--<script src="<?php echo base_url('') ?>assets/js/bootstrap.min.js"></script>-->
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
  		<script src="Hello World"></script>
-	</body>
+		
+		<script>
+	$(document).ready(function () {
+	var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
+</script>
+</body>
 </html>
